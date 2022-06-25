930c8d2c569132856daf78079900bf66bfff8a10
cmschultz <caioschultz@unifei.edu.br>
2022-06-25 18:32:28 -0300

    .

--- led.py
+++ led.py
@@ -6,19 +6,19 @@ GPIO.setmode(GPIO.BOARD)   #Configura o modo de definição de pinos como BOARD 
 
 GPIO.setwarnings(False)           #Desativa os avisos 
 
-GPIO.setup(18, GPIO.OUT)       #Configura o pino 18 da placa (GPIO24) como saída
+GPIO.setup(7, GPIO.OUT)       #Configura o pino 18 da placa (GPIO24) como saída
 
 while(1):                                   #Inicia o loop infinito
 
     print("LED ACESO\n")          #Escreve na Python Shell a mensagem "LED ACESO" e pula uma linha
 
-    GPIO.output(18, 1)               #Coloca o pino 18 em nível alto (1)
+    GPIO.output(7, 1)               #Coloca o pino 18 em nível alto (1)
 
     time.sleep(1)                        #Delay de 1 segundo
 
     print("LED APAGADO\n")      #Escreve na Python Shell a mensagem "LED APAGADO" e pula uma linha
 
-    GPIO.output(18, 0)               #Coloca o pino 18 em nível baixo (0)
+    GPIO.output(7, 0)               #Coloca o pino 18 em nível baixo (0)
 
     time.sleep(1)                        #Delay de 1 segundo
 
